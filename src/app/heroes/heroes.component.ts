d5b97ac09c5c950e03d9d8d54f6dbacc88d0aac0
SEGAT0 <richard.segato.brides@gmail.com>
2022-10-04 11:13:11 -0300

    aula 04/10

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,3 +1,4 @@
+import { HEROES } from './../mock-heroes';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 
@@ -6,17 +7,21 @@ import { Hero } from '../hero';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSelect(hero: Hero): void{
+  this.selectedHero = hero;
   }
 
+  }
   
-}
+
+  
